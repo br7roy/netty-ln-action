ca837724736dba8139040e4c289a3201813d03ba
Rust <br7roy@gmail.com>
2018-11-10 10:34:30 +0800

    Initial commit

---
+++ nettydemo/src/main/java/com/rust/heartbeat/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * FileName: package-info
+ * Author:   Rust
+ * Date:     2018/6/21
+ * Description: netty heartbeat
+ * History:<a https://waylau.com/netty-time-out-and-heartbeat/>
+ */
+package com.rust.heartbeat;
